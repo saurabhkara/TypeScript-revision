f97ea1af07b1bfbd1757e081f4560d03c7ce90b6
Saurabh <saurabkumar679@gmail..com>
2024-10-15 08:20:37 +0530

    types and interfaces

---
+++ IntrfaceAndTypes.ts
@@ -0,0 +1,70 @@
+//Implicit infered type
+const inferredNumber = 25;
+
+//types 
+type TUser = {
+    name: string
+}
+
+const User: TUser = {
+    name: 'Saurabh'
+}
+
+
+//intersection operation on types
+
+type TCar = {
+    make: string;
+    model: string;
+}
+
+type TAdvanceCar = TCar & {
+    isElectric: true
+}
+
+const AdvanceCar: TAdvanceCar = {
+    make: 'Tesla',
+    model: 'T',
+    isElectric: true
+}
+
+// Union opersion - or
+
+type TPerson = {
+    name: string
+} | string;
+
+const Person: TPerson = 'Saurabh';
+
+
+
+//Interface
+
+//Can be extensionsiable 
+interface IAnimal {
+    livingBeing: true
+}
+
+interface IHuman extends IAnimal {
+    homosapian: boolean
+}
+
+const Human: IHuman = {
+    livingBeing: true,
+    homosapian: true
+}
+
+//Declaration merging
+
+interface ILaptop {
+    company: string
+}
+
+interface ILaptop {
+    model: string
+}
+
+const Laptop: ILaptop = {
+    company: 'apple',
+    model: 'm3'
+}
